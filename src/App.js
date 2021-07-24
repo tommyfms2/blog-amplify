e81ebce5463119d4164de12cfb80ffe15333006c
Tomihiro Toyozaki <tomihirotoyozaki@Tomihiros-MacBook-Air.local>
2021-07-24 16:17:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import DisplayPosts from './components/DisplayPosts';
+import CreatePost from './components/CreatePost';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifySignOut />
+      < CreatePost />
+      < DisplayPosts />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
